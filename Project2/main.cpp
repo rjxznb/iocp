4a4a2bd833d08696f7a67ff31cf9b658e754d014
家兴 任 <2216681472@qq.com>
2025-02-13 15:07:02 +0800

    v1.2

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -10,8 +10,23 @@ using namespace std;
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Mswsock.lib")
 
-
+class A {
+public:
+	A(int a, int b) :a{ a }, b{b} {
+		cout << "A" << endl;
+	}
+	int a;
+	int b;
+};
 
 int main() {
-	CreateIoCompletionPort();
+	STARTUPINFOW info{};
+	memset(&info, 0, sizeof(info));
+	info.cb = sizeof(info);
+	PROCESS_INFORMATION pinfo{};
+	memset(&pinfo, 0, sizeof(pinfo));
+	TCHAR cmdline[] = L"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe";
+	if (CreateProcess(NULL, cmdline, NULL, NULL, false, CREATE_NEW_CONSOLE, NULL, NULL, &info, &pinfo)) {
+		WaitForSingleObject(pinfo.hProcess, INFINITE);
+	}
 }

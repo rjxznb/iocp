c6efda2d9f69b1cda8daed1599b67f7a827a2dab
renjx <renjx@RENJX>
2025-02-13 10:00:03 +0800

    添加项目文件。

---
+++ Project2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mswsock.h>
+#include <stdio.h>
+#include<vector>
+#include<string>
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+
+
+
+int main() {
+	CreateIoCompletionPort
+}
